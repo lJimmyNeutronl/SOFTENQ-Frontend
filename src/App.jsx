a6c6971950ae4b7617a359fcb0e7f0c2045c4c9e
lJimmyNeutronl <ljimmyneytronl@gmail.com>
2023-10-12 10:34:24 +0600

    Task 1(proxy)

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   const [data, setData] = useState("");
 
   const getData = async()=>{
-    const response = await Axios.get("http://localhost:3000/getData");
+    const response = await Axios.get("http://localhost:8000/api");
     setData(response.data);
   }
 
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server : {
-    port : 8000
+    port : 8000,
+    proxy: {
+      '/api' : {
+        target : 'http://localhost:3000/getData',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    },
   },
   plugins: [react()],
 })
