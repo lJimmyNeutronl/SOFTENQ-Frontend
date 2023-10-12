9fa5f968bfcc843f4d56d20d47963290fa9d181a
lJimmyNeutronl <ljimmyneytronl@gmail.com>
2023-10-12 10:55:38 +0600

    Task 1(proxy) - Modified

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port : 8000,
     proxy: {
       '/api' : {
-        target : 'http://localhost:3000/getData',
+        target : 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
